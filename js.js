6b89d63888d99560cec2ce62657839b67739c069
sai-Sweshika <saisweshikakodurupaka@gmail.com>
2024-11-09 20:26:26 +0530

    I have practiced the functions

---
+++ js.js
@@ -0,0 +1,15 @@
+function a() {
+    var k = document.getElementById("p")
+    k.style.backgroundColor = "red";
+    k.style.borderRadius = "30%";
+    k.style.fontSize = "50px"
+    k.style.alignItems = "center"
+    k.style.display = "inline-block"
+}
+function b() {
+    var s = document.getElementById("p1");
+    s.innerHTML = `<img src="https://th.bing.com/th/id/OIP.mzvDptAzgH55MKzIcPzTsgHaFj?w=266&h=200&c=7&rs=1&dpr=1.5&pid=1.7" style="border-radius: 30px; margin-right: 10px;" height="200px" width="200px">
+    <img src="https://th.bing.com/th/id/OIP.9_0V54kmBlqqDQizscxzlQHaEK?w=309&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7" style="border-radius: 50px; margin-right: 10px;" height="200px" width="200px">
+    <img src="https://th.bing.com/th/id/OIP.defmtwl1SFwOTbtE3mV8cAHaEc?w=307&h=184&c=7&r=0&o=5&dpr=1.5&pid=1.7s" style="border-radius:100px; margin-right: 10px;" height="200px" width="200px">`
+        ;
+}
